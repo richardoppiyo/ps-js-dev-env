3764e9887313a94e27956324e20e4a27848e41d3
unknown <richardoppiyo@gmail.com>
2019-06-25 17:56:34 +0530

    css added, which is linked in the js fire. weird! It works though

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+import numeral from 'numeral';
+
+const courseVAlue = numeral(1000).format('$0,0.00');
+console.log(`I would pay ${courseVAlue} for this awesome Pluralsight course`);
