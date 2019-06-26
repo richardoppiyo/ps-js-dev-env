3e3eca60fab57388cd54413da0686bd4cbc2fa2a
unknown <richardoppiyo@gmail.com>
2019-06-26 15:03:54 +0530

    Linting: added eslint for rapid feedback. eslint-watch for to l

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseVAlue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseVAlue} for this awesome Pluralsight course`);
+console.log(`I would pay ${courseVAlue} for this awesome Pluralsight course`); // eslint-disable-line no-console
